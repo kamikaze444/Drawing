fc0840afcbbadc93c77875540d20741bbd53bb76
kamikaze444 <megaluis.soto.gonzalez@gmail.com>
2026-06-26 13:26:43 +0200

    modified:   drawing.py

--- drawing.py
+++ drawing.py
@@ -12,7 +12,7 @@ class Main():
     def __init__(self):
         root = tkinter.Tk()
         self.dis_sizes = (root.winfo_screenheight(),root.winfo_screenheight())
-        
+        hello
         self.length,self.height = (900,900)
         self.screen = pg.display.set_mode(self.length,self.height)
 
